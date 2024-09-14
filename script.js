059f140d0bfce779b0e11927239839e6a15253fc
Huzaifa986 <umaisahmed89@gmail.com>
2024-09-14 11:50:50 -0700

    java intro

---
+++ script.js
@@ -0,0 +1,20 @@
+//alert
+alert("allow this websites changes");
+
+//var +
+var a = 10;
+var b = 30;
+var c = ( a + b);
+console.log(c);
+    
+//var -
+var a = 12;
+var b = 10;
+var c =(a-b);
+console.log(c) 
+
+//var *
+var a = 5;
+var b = 8;
+var c = (5*4);
+console.log(c)
